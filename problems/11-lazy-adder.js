e6a18bbab3f104c8d72802043b08b5cecc2187e9
Adam Elhamami <adamel99@gmail.com>
2023-02-02 19:16:01 -0500

    11

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -22,7 +22,16 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   fat-arrow function
 ***********************************************************************/
 
-// Your code here
+let lazyAdder = (firstNum) => (secondNum) => (thirdNum) => (firstNum + secondNum + thirdNum)
+
+
+function lazyAdder (firstNum){
+  return function (secondNum){
+    return function (thirdNum){
+      return firstNum + secondNum+ thirdNum;
+    }
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
